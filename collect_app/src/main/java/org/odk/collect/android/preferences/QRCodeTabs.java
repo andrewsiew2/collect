466d04a548839de4887fe5894c3146a8d0ec1f42
stlp <stlp@lc-926.hsd1.wa.comcast.net>
2020-03-04 10:50:55 -0800

    moved some functions to qrcode tabs

--- collect_app/src/main/java/org/odk/collect/android/preferences/QRCodeTabs.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/QRCodeTabs.java
@@ -2,6 +2,7 @@ package org.odk.collect.android.preferences;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -14,11 +15,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+
+import org.odk.collect.android.BuildConfig;
 import org.odk.collect.android.R;
 import org.odk.collect.android.activities.CollectAbstractActivity;
 import org.odk.collect.android.adapters.TabAdapter;
 import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.fragments.ShowQRCodeFragment;
+import org.odk.collect.android.utilities.FileUtils;
+import org.odk.collect.android.utilities.QRCodeUtils;
 import org.odk.collect.android.utilities.ThemeUtils;
 import org.odk.collect.android.utilities.ToastUtils;
 
@@ -39,6 +44,7 @@ import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -51,6 +57,7 @@ public class QRCodeTabs extends CollectAbstractActivity {
     private TabAdapter adapter;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private Intent shareIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,17 +67,29 @@ public class QRCodeTabs extends CollectAbstractActivity {
         viewPager = (ViewPager) this.findViewById(R.id.viewPager);
         tabLayout = (TabLayout) this.findViewById(R.id.tabLayout);
         adapter = new TabAdapter(getSupportFragmentManager());
-        adapter.addFragment(new ShowQRCodeFragment(), "Tab 1");
-        adapter.addFragment(new ShowQRCodeFragment(), "Tab 2");
+        adapter.addFragment(new ShowQRCodeFragment(), "QR code");
+        adapter.addFragment(new ShowQRCodeFragment(), "Scan");
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+        this.updateShareIntent();
     }
 
 
     private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setTitle(getString(R.string.import_export_settings));
+        setTitle(getString(R.string.qr_code));
         setSupportActionBar(toolbar);
+    }
+
+    private void updateShareIntent() {
+        // Initialize the intent to share QR Code
+        shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("image/*");
+        Uri uri =
+                FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", new File(QRCodeUtils.getQrCodeFilepath()));
+        FileUtils.grantFileReadPermissions(shareIntent, uri, this);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
     }
 
 
@@ -78,5 +97,17 @@ public class QRCodeTabs extends CollectAbstractActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.settings_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_share:
+                if (shareIntent != null) {
+                    startActivity(Intent.createChooser(shareIntent, getString(R.string.share_qrcode)));
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
